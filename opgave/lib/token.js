002755c93448ceb5698eda83079e5c3e141bd703
FranValcke <fran.valcke@student.kdg.be>
2018-12-05 19:36:19 +0100

    first commit

--- opgave/lib/token.js
+++ opgave/lib/token.js
@@ -1,23 +1,27 @@
 /**
  * Created by Fran on 5/12/2018.
  */
-const Web3 = require('web3')
-const path = require('path')
-const cjson = require('cjson')
-const TX = require('ethereumjs-tx')
-// contract details
+const Web3 = require('web3');
+const path = require('path');
+const cjson = require('cjson');
+const TX = require('ethereumjs-tx');
 
+
+// contract details
 //Deze 5 parameters heb ik aangepast naar mijn contract
 
-const provider = 'https://rinkeby.infura.io/v3/ee4d4f4d70a7439a973da7221d48dad8'
-const contractAddress = '0xb075fecb70e9313b23691d50227ef1d2a21fa214'
-const privateKey = new Buffer('2095c47da2d0d2c221e65e31d00c1590b217bdd3df9b5404e1a570c805f72649', 'hex')
-const defaultAccount = '0x246cf707AD4C3c02fB16D540aA0B5E45f4CF145B'
-const etherscanLink = 'https://rinkeby.etherscan.io/tx/0x8d3947e7b9a25021b5d28b45dd8365eaf1f097f4e4ea761e06177cd6307678ad'
+const provider = 'https://rinkeby.infura.io/v3/ee4d4f4d70a7439a973da7221d48dad8';
+const contractAddress = '0xb075fecb70e9313b23691d50227ef1d2a21fa214';
+const privateKey = new Buffer('2095c47da2d0d2c221e65e31d00c1590b217bdd3df9b5404e1a570c805f72649', 'hex');
+const defaultAccount = '0x246cf707AD4C3c02fB16D540aA0B5E45f4CF145B';
+const etherscanLink = 'https://rinkeby.etherscan.io/tx/0x8d3947e7b9a25021b5d28b45dd8365eaf1f097f4e4ea761e06177cd6307678ad';
+
 // initiate the web3
-const web3 = new Web3(provider)
+const web3 = new Web3(provider);
+
 // initiate the contract with null value
 var contract = null;
+
 // convert Wei to Eth
 function convertWeiToEth( stringValue ) {
     if ( typeof stringValue != 'string' ) {
@@ -25,22 +29,24 @@ function convertWeiToEth( stringValue ) {
     }
     return web3.utils.fromWei( stringValue, 'ether' );
 }
+
 // Initiate the Contract
 function getContract() {
     if (contract === null) {
         var abi = cjson.load(path.resolve(__dirname, '../ABI/abi.json'));
-        var c = new web3.eth.Contract(abi,contractAddress)
+        var c = new web3.eth.Contract(abi,contractAddress);
         contract = c.clone();
     }
-    console.log('Contract Initiated successfully!')
+    console.log('Contract Initiated successfully!');
     return contract;
 }
+
 // send token to Address
 async function sendToken(req, res) {
-    var address = req.body.address
-    var tokens = Number(req.body.tokens)
+    var address = req.body.address;
+    var tokens = Number(req.body.tokens);
     if (address && tokens) {
-        const rawTrans = getContract().methods.send(address, tokens) // contract method
+        const rawTrans = getContract().methods.send(address, tokens) ;// contract method
         return res.send(await sendSignTransaction(rawTrans))
     } else {
         res.send({
@@ -48,12 +54,13 @@ async function sendToken(req, res) {
         })
     }
 }
+
 // Mint/Create token to given address
 async function mintToken(req, res) {
-    var address = req.body.address
-    var tokens = Number(req.body.tokens)
+    var address = req.body.address;
+    var tokens = Number(req.body.tokens);
     if (address && tokens) {
-        const rawTrans = getContract().methods.mint(address, tokens) // contract method
+        const rawTrans = getContract().methods.mint(address, tokens) ;// contract method
         return res.send(await sendSignTransaction(rawTrans))
     } else {
         res.send({
@@ -63,7 +70,7 @@ async function mintToken(req, res) {
 }
 // get the balance of given address
 async function getBalance(req, res) {
-    var address = req.query.address
+    var address = req.query.address;
     if (address) {
         // get the Ether balance of the given address
         var ethBalance = convertWeiToEth( await web3.eth.getBalance(address)) || '0'
@@ -81,14 +88,14 @@ async function getBalance(req, res) {
 async function sendSignTransaction(rawTrans) {
     // Initiate values required by the dataTrans
     if (rawTrans) {
-        var txCount = await web3.eth.getTransactionCount(defaultAccount) // needed for nonce
-        var abiTrans = rawTrans.encodeABI() // encoded contract method
+        var txCount = await web3.eth.getTransactionCount(defaultAccount) ;// needed for nonce
+        var abiTrans = rawTrans.encodeABI() ;// encoded contract method
 
-        var gas = await rawTrans.estimateGas()
-        var gasPrice = await web3.eth.getGasPrice()
-        gasPrice = Number(gasPrice)
-        gasPrice = gasPrice * 2
-        var gasLimit = gas * 4
+        var gas = await rawTrans.estimateGas();
+        var gasPrice = await web3.eth.getGasPrice();
+        gasPrice = Number(gasPrice);
+        gasPrice = gasPrice * 2;
+        var gasLimit = gas * 4;
 // Initiate the transaction data
         var dataTrans = {
             nonce: web3.utils.toHex(txCount),
@@ -96,10 +103,10 @@ async function sendSignTransaction(rawTrans) {
             gasPrice: web3.utils.toHex(gasPrice),
             to: contractAddress,
             data: abiTrans
-        }
+        };
         // sign transaction
-        var tx = new TX(dataTrans)
-        tx.sign(privateKey)
+        var tx = new TX(dataTrans);
+        tx.sign(privateKey);
 // after signing send the transaction
         return await sendSigned(tx)
     } else {
@@ -126,11 +133,11 @@ function sendSigned(tx) {
         .catch(err => {
             // respond with error
             reject(err)
-        })
+            })
     })
 }
 module.exports = {
     send: sendToken,
     mint: mintToken,
     balance: getBalance
-}
+};
